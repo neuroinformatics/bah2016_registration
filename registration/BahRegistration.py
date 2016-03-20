c2b73dc3641d2732c8a6f1b38cf2026f0bcd58a9
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2016-03-21 04:50:57 +0900

    show metric value

--- registration/BahRegistration.py
+++ registration/BahRegistration.py
@@ -6,53 +6,84 @@ import sys
 
 class BahRegistration:
 
-    def __init__(self, homedir = None):
+    def __init__(self,
+                 homedir = None,
+                 reg_program = None
+                 ):
+
         if homedir is None:
             homedir = os.path.join('/', 'data', 'registration', 'Processed')
-
         if not os.path.exists(homedir):
             sys.exit('Error : %s not found.' % homedir)
 
+        if reg_program is None:
+            reg_program = '/share/apps/registration/itk/ImageRegistration/ImageRegistration9'
+        if not os.path.exists(reg_program):
+            sys.exit('Error : %s not found.' % reg_program)
+
+
         self.homedir = homedir
-        self.sbfile_base = os.path.join(homedir, 'SB_bin/Reslice_WHS_0.6.1_Labels_fixed_bin%04d.tif')
+        self.reg_program = reg_program
+        self.sbfile_base = os.path.join(homedir, 'SB_bin', 'Reslice_WHS_0.6.1_Labels_fixed_bin%04d.tif')
         self.registration_in = os.path.join(homedir, 'work3')
         self.registration_out = os.path.join(homedir, 'work4')
-        self.registration_log = os.path.join(homedir + 'log4')
+        self.registration_log = os.path.join(homedir,'log4')
+        self.registration_metric = os.path.join(homedir, 'metric')
         
         if not os.path.exists(self.registration_in):
             sys.exit('Error : %s not found.' % self.registration_in)
 
+        
+        # output dirs
         if not os.path.exists(self.registration_out):
             os.mkdir(self.registration_out)
         if not os.path.exists(self.registration_log):
             os.mkdir(self.registration_log)
+        if not os.path.exists(self.registration_metric):
+            os.mkdir(self.registration_metric)
 
     
     def single_registration(self, filename,
-                            reg_program = '/share/apps/registration/itk/ImageRegistration/ImageRegistration9',
                             sb_reslice_start=80, sb_reslice_end=121):
 
         filename_base, filename_ext = os.path.splitext(filename)
-        logdir = self.registration_log + filename_base + '/'
+        outdir = os.path.join(self.registration_out, filename_base)
+        logdir = os.path.join(self.registration_log, filename_base)
+        metricfilename = os.path.join(self.registration_metric, filename_base+'.txt')
+        result_metric = {}
+
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
 
         if not os.path.exists(logdir):
             os.mkdir(logdir)
+
         for i in range(sb_reslice_start, sb_reslice_end):
             sbfile = self.sbfile_base % i
             inputfile = os.path.join(self.registration_in, filename)
-            outputfile = os.path.join(self.registration_out, filename_base+('_%04d.tif' % i))
+            outputfile = os.path.join(outdir, ('%04d.tif' % i))
+            logfilename = os.path.join(logdir, ('%04d.log' % i))
             
-            cmd = [reg_program, sbfile, inputfile, outputfile]
+            cmd = [self.reg_program, sbfile, inputfile, outputfile]
             print cmd
 
             ret = subprocess.check_output(cmd)
+            result = ret.split('\n')
 
-            logfilename = logdir + ('%04d.log' % i)
             with open(logfilename, mode='w') as f:
-                for line in ret:
+                for line in result:
                     f.write(line)
+                    if 'Metric value' in line:
+                        line_split = line.split('=')
+                        result_metric[i] = line_split[1]
 
+        with open(metricfilename, mode='w') as f:
+            for k, v in result_metric.items():
+                f.write(str(k)+', '+v)
 
+        return result_metric
+
+    
 
 
 if __name__ == '__main__':
--- registration/para_registration.py
+++ registration/para_registration.py
@@ -1,67 +1,59 @@
-#!/usr/bin/env python
-#coding: UTF-8
+# -*- coding: utf-8 -*-
 
+import os
+import sys
 from ipyparallel import Client
 
-def singlejob(arg):
-    import subprocess
-    import os
 
-    filename = arg
-    filename_base, filename_ext = os.path.splitext(filename)
-    homedir = '/data/registration/Processed/'
-    sbfile_base  = homedir + 'SB_bin/Reslice_WHS_0.6.1_Labels_fixed_bin%04d.tif'
-    indir  = homedir + 'work3/'
-    outdir = homedir + 'work4/'
-    logdir = homedir + 'log4/' + filename_base + '/'
-    
-    RegProgram1 = '/share/apps/registration/itk/ImageRegistration/ImageRegistration9'
-    command_format = RegProgram1 + ' %s %s %s'
-
-    os.mkdir(logdir)
-
-    for i in range(80, 121):
-        sbfile = sbfile_base % i
-        cmd = [RegProgram1, sbfile, indir+filename, outdir+filename_base+('_%04d.tif' % i)]
-        print cmd
-
-        ret = subprocess.check_output(cmd)
-
-        logfilename = logdir + ('%04d.log' % i)
-        with open(logfilename, mode='w') as f:
-            for line in ret:
-                f.write(line)
+class ParaExec:
+ 
+    def __init__(self, addpath=None):
+        self.client = Client()
+        self.load_balanced_view = self.client.load_balanced_view()
+        if(len(self.client.ids) == 0):
+            print('# of engines : single mode')
+        else:
+            print('# of engines : %d' % len(self.client.ids))
 
 
-def do_parallel(filelist):
-    rc = Client()
+    def do_parallel(self, func, arglist):
+        print('# of job : %d' % len(arglist))    
 
-    print('# of engines : %d' % len(rc.ids))
-    print('# of job : %d' % len(filelist))
-
-    lv = rc.load_balanced_view()
-    result = lv.map_async(singlejob, filelist)
-    result.wait_interactive()
+        rs = self.load_balanced_view.map_async(func, arglist)
+        rs.wait_interactive()
+        print rs.result()
 
 
-def do_single(filelist):
-    print('# of engines : single')
-    print('# of job : %d' % len(filelist))
-
-    for arg in filelist:
-        singlejob(arg)
+    def do_single(self, func, arglist):
+        print('# of job : %d' % len(arglist))
+        for arg in arglist:
+            func(arg)
 
 
 if __name__ == '__main__':
-    filelist_file = '/data/registration/filelist.txt'
-    #filelist_file = '/data/registration/filelist_small.txt'
+    def wrap_registration(filename):
+        import os,sys
+        sys.path.append('/data/miyamoto/git/bah2016_registration/registration')
 
+        import BahRegistration
+        reload(BahRegistration)
 
-    filelist = []
-    with open(filelist_file, mode='r') as f:
-        filelist = f.readlines()
+        reg = BahRegistration.BahRegistration(homedir='/data/registration/Test')
+        metric = reg.single_registration(filename, sb_reslice_end=83)
 
-    filelist = [str.rstrip() for str in filelist]
+        return metric
 
-    do_parallel(filelist)
-    #do_single(filelist)
+    def read_filelist(filelistname):
+        filelist = []
+        with open(filelistname, mode='r') as f:
+            filelist = f.readlines()
+
+        filelist = [str.rstrip() for str in filelist]
+        return filelist
+
+    filelistname = os.path.join('/', 'data', 'registration', 'Test', 'filelist_test.txt')
+    filelist = read_filelist(filelistname)
+
+    pec = ParaExec()
+    pec.do_parallel(wrap_registration, filelist)
+    #pec.do_single(wrap_registration, filelist)
